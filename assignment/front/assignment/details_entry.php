e01c62e84d071a3098de8df9d4a3b17cb2cfb6b7
irfanhzre <irfanhazarie@gmail.com>
2023-06-21 02:50:15 +0800

    done

--- assignment/front/assignment/details_entry.php
+++ assignment/front/assignment/details_entry.php
@@ -1,4 +1,18 @@
 <?php
+// Connect to the database
+$servername = "localhost";
+$username = "user";
+$password = "userpwd";
+$database = "melewar";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check the database connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve form data
@@ -22,29 +36,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo '<br>'.'<br>'.'masa'.$time;
     
     // TODO: Perform input validation and sanitization
+
+    $query = "SELECT * FROM reservations WHERE date = '$date' AND time = '$time' AND table_number = '$tableNumber'";
+
+    // Execute the query
+    $result = mysqli_query($conn, $query);
+
+    // Check if the row exists
+    if (mysqli_num_rows($result) > 0) {
+    // The row exists, show the pop-up message or perform the desired action
+    echo "<script>alert('The table already book!'); window.location.href = 'reservation.php';</script>";
     
-    // Connect to the database
-    $servername = "localhost";
-    $username = "user";
-    $password = "userpwd";
-    $database = "melewar";
-    
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    
-    // Check the database connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    } else {
+    // The row does not exist
+    echo "<script>alert('The row does not exist!');</script>";
+
+    $sql = "INSERT INTO reservations (name, email, phone, date, time, guests, table_number, item1, item1_quantity, item2, item2_quantity, item3, item3_quantity, item4, item4_quantity, item5, item5_quantity)
+    VALUES ('$name', '$email', '$phone', '$date', '$time', '$guests', '$tableNumber', '$item1', '$item1Quantity', '$item2', '$item2Quantity', '$item3', '$item3Quantity', '$item4', '$item4Quantity', '$item5', '$item5Quantity')";
+
+    if (mysqli_query($conn, $sql)) {
+    echo "Reservation successfully created.";
+    header("Location: reservation.php");
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
     }
     
     // Insert the data into the reservations table
-    $sql = "INSERT INTO reservations (name, email, phone, date, time, guests, table_number, item1, item1_quantity, item2, item2_quantity, item3, item3_quantity, item4, item4_quantity, item5, item5_quantity)
-            VALUES ('$name', '$email', '$phone', '$date', '$time', '$guests', '$tableNumber', '$item1', '$item1Quantity', '$item2', '$item2Quantity', '$item3', '$item3Quantity', '$item4', '$item4Quantity', '$item5', '$item5Quantity')";
     
-    if (mysqli_query($conn, $sql)) {
-        echo "Reservation successfully created.";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
     
     // Close the database connection
     mysqli_close($conn);
